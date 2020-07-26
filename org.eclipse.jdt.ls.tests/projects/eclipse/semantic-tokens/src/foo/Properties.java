88504078d30035687fe530d3584fdb6446bbc029
0dinD <zerodind@gmail.com>
2020-07-26 23:32:41 +0200

    Add TokenAssertionHelper
    Signed-off-by: 0dinD <zerodind@gmail.com>

--- org.eclipse.jdt.ls.tests/projects/eclipse/semantic-tokens/src/foo/Properties.java
+++ org.eclipse.jdt.ls.tests/projects/eclipse/semantic-tokens/src/foo/Properties.java
@@ -4,7 +4,7 @@ public class Properties {
 
 	public String bar1;
 	private int bar2;
-	protected boolean bar3;
+	protected boolean bar3 = bar2;
 	final String bar4;
 	static int bar5;
 	public static final int bar6 = 1;
